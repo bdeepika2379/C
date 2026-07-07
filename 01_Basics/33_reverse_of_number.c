d819bd1dcd46590d80d9487891e02754911992c6
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 16:37:39 +0530

    Create 33_reverse_of_number.c

---
+++ 01_Basics/33_reverse_of_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    while(number != 0)
+    {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+    }
+
+    printf("Reverse of number = %d", reverse);
+
+    return 0;
+}
