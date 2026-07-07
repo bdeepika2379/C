9a1faf4ca162aab7afa1ece903a37fee419e135b
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 16:29:26 +0530

    Create 28_divisible_by_5_and_11.c

---
+++ 01_Basics/28_divisible_by_5_and_11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(number % 5 == 0 && number % 11 == 0)
+    {
+        printf("The number is divisible by 5 and 11");
+    }
+    else
+    {
+        printf("The number is not divisible by 5 and 11");
+    }
+
+    return 0;
+}
