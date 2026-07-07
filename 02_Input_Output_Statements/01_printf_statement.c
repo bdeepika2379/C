5a02369a2d3213740b92abb86ebd2ce43b6fe846
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 17:22:35 +0530

    Create 01_printf_statement.c

---
+++ 02_Input_Output_Statements/01_printf_statement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    printf("Welcome to C Programming\n");
+
+    printf("I am learning printf statement");
+
+    return 0;
+}
