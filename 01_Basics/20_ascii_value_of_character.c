7f2ce5d464891eaec91c4fe197b0e8f7e4450f4d
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 16:22:35 +0530

    Create 20_ascii_value_of_character.c

---
+++ 01_Basics/20_ascii_value_of_character.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char character;
+
+    printf("Enter a character: ");
+    scanf("%c", &character);
+
+    printf("ASCII value of %c = %d", character, character);
+
+    return 0;
+}
