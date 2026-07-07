cfe24cd6179cf805f8e33a00bd910aecb9d1fca8
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 16:11:20 +0530

    Create 09_Square_of_a_Number.c

---
+++ 01_Basics/09_Square_of_a_Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, square;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    square = num * num;
+
+    printf("Square = %d", square);
+
+    return 0;
+}
