63be65f1196dee5d1a008c4fac761922ee2dd64b
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-07 16:38:46 +0530

    Create 34_palindrome_number.c

---
+++ 01_Basics/34_palindrome_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, original, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    original = number;
+
+    while(number != 0)
+    {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+    }
+
+    if(original == reverse)
+    {
+        printf("The number is a Palindrome");
+    }
+    else
+    {
+        printf("The number is not a Palindrome");
+    }
+
+    return 0;
+}
