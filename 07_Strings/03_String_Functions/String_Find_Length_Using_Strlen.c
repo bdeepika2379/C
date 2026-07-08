5925db420c02f524f55e0ee9104ac4a831ab93fb
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-08 20:02:23 +0530

    Create String_Find_Length_Using_Strlen.c

---
+++ 07_Strings/03_String_Functions/String_Find_Length_Using_Strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int length;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    length = strlen(str);
+
+    printf("Length of the string: %d", length - 1);
+
+    return 0;
+}
