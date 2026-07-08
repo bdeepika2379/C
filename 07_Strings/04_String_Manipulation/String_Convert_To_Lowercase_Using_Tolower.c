cf21af7bdc109aa77dfd4b6884a2454c36a29ab8
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-08 20:08:02 +0530

    Create String_Convert_To_Lowercase_Using_Tolower.c

---
+++ 07_Strings/04_String_Manipulation/String_Convert_To_Lowercase_Using_Tolower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0')
+    {
+        str[i] = tolower(str[i]);
+        i++;
+    }
+
+    printf("Lowercase string: %s", str);
+
+    return 0;
+}
