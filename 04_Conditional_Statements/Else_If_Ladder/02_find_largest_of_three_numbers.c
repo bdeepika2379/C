b69ab58c8fdd46edad5d45e39f7969993e891b9d
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-08 17:22:49 +0530

    Create 02_find_largest_of_three_numbers.c

---
+++ 04_Conditional_Statements/Else_If_Ladder/02_find_largest_of_three_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a >= b && a >= c)
+    {
+        printf("%d is the largest number", a);
+    }
+    else if(b >= a && b >= c)
+    {
+        printf("%d is the largest number", b);
+    }
+    else
+    {
+        printf("%d is the largest number", c);
+    }
+
+    return 0;
+}
+
+/*
+Else-If Ladder
+
+This program finds the largest
+among three numbers using
+if, else if, and else.
+*/
