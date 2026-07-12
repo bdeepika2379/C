c5ee6a3d0025dd8b7d0f2208c928251b9171ad61
bdeepika2379 <bdeepika2379@gmail.com>
2026-07-12 10:37:55 +0530

    Create 08_Function_Display_Message.c

---
+++ 08_Functions/01_User_Defined_Functions/08_Function_Display_Message.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function Declaration
+void displayMessage();
+
+int main()
+{
+    // Function Call
+    displayMessage();
+
+    return 0;
+}
+
+// Function Definition
+void displayMessage()
+{
+    printf("Welcome to C Programming!");
+}
